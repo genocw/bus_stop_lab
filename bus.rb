d742c4a6039763e79195e6cc81d2e48cc26203fd
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-09-18 16:03:56 +0100

    Add function to pick up person

--- bus.rb
+++ bus.rb
@@ -1,15 +1,21 @@
 class Bus
 
   attr_reader :route, :last_stop
+  attr_accessor :passengers
 
   def initialize(route_number, last_stop)
     @route = route_number
     @last_stop = last_stop
+    @passengers = []
   end
 
+  def count()
+    return @passengers.length()
+  end
 
-
-
+  def pick_up(person)
+    @passengers.push(person)
+  end
 
 
 
--- specs/bus_test.rb
+++ specs/bus_test.rb
@@ -2,6 +2,7 @@ require("minitest/autorun")
 require("minitest/rg")
 require_relative("../bus.rb")
 require_relative("../busstop.rb")
+require_relative("../person.rb")
 
 
 class BusTest < MiniTest::Test
@@ -20,9 +21,23 @@ class BusTest < MiniTest::Test
     assert_equal("Ocean Terminal", @bus22.last_stop())
   end
 
-  def test_bus_can_drive()
-    
+  # def test_bus_can_drive()
+  #   assert_equal("")
+  # end
+
+  def test_count()
+    assert_equal(0, @bus22.count())
   end
+
+
+  def test_pick_up_person()
+    person = Person.new("David Bowie", 65)
+    @bus22.pick_up(person)
+    assert_equal(1, @bus22.count())
+  end
+
+
+
 
 
 
